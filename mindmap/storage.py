7aa3d87970ae668344bf64a0d5ef2f922b394c9d
Joris Bailly <joris.bailly.2001@gmail.com>
2025-04-29 16:21:02 +0200

    Improve the save

--- mindmap/storage.py
+++ mindmap/storage.py
@@ -1,3 +1,5 @@
+import os
+
 import yaml
 
 from mindmap.models import  Node
@@ -9,11 +11,23 @@ def to_dict(node: Node):
     }
 
 def save_mindmap(root_node: Node,filename: str):
-    with open(filename, "w") as f:
+    if '.yml' not in filename:
+        filename += '.yml'
+
+
+    with open(get_filepath(filename), "w") as f:
         yaml.dump(to_dict(root_node), f, allow_unicode=True)
 
 def load_mindmap(filename: str):
-    with open(filename, "r") as f:
+    if '.yml' not in filename:
+        filename += '.yml'
+    with open(get_filepath(filename), "r") as f:
         data = yaml.safe_load(f)
         return Node.from_dict(data)
 
+
+def get_filepath(filename:str):
+    data_dir = "data"
+    os.makedirs(data_dir, exist_ok=True)
+    full_path = os.path.join(data_dir, filename)
+    return full_path
